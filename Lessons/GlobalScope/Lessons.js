0845bdcb6dbcd71fa2879d8b8363840befb5404e
oliverforster <oliforster798@gmail.com>
2019-11-28 13:51:21 +0000

    FirstCommit

---
+++ Lessons/GlobalScope/Lessons.js
@@ -0,0 +1,10 @@
+//declare global variables
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+//function to call variables
+function callMyNightSky() {
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+
+console.log(callMyNightSky())
